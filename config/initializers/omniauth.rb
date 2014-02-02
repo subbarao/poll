2aee5c26e36c72053a61867b2507654d4d01f6f6
Subba Rao Pasupuleti <subbarao.pasupuleti@gmail.com>
2014-02-02 16:26:00 -0500

    initial commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '162180660488445', 'ddd5b1dd54b2eb99fb9b79835d1a0fc8'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pfl::Application.config.secret_key_base = '990179ae858bfb2b8f58df9736db5c6fdbeb6c2e16dd967f3fc8d51c1b6a74b7bbddf2e52be886a048ff51bdb91d4d26936ff9c9a675fcf09e05ac7ff94ed407'
