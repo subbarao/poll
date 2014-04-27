36f9147dede3b6c19a19c829dc9ebd5639744c7c
Subba Rao Pasupuleti <subbarao.pasupuleti@gmail.com>
2014-04-27 10:18:29 -0400

    adding grand total

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '638223849586471', '722b0597979cd66167349eb47643ec27'
+  #provider :facebook, '638223849586471', '722b0597979cd66167349eb47643ec27'
+  provider :facebook, '162180660488445', 'ddd5b1dd54b2eb99fb9b79835d1a0fc8'
 end
