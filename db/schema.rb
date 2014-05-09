2895694a9be781a5c8bcd7f142c9fa6794a4033c
Prabakar Battula <prabhakar.battula@gmail.com>
2014-05-09 19:51:20 +0530

    changed 'income_for_2012_2013' column data type from text to integer in candidate table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140509132508) do
+ActiveRecord::Schema.define(version: 20140509135950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20140509132508) do
     t.string   "spouse_name"
     t.string   "education"
     t.string   "occupation"
-    t.string   "income_for_2012_2013"
+    t.integer  "income_for_2012_2013"
     t.integer  "nominations_id"
     t.datetime "created_at"
     t.datetime "updated_at"
